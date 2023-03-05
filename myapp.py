a05a6e1465569e49d2cdacd62f1794702e17c99a
koremoritaira <113714154+koremoritaira@users.noreply.github.com>
2023-03-05 18:52:55 +0200

    Update myapp.py
    Fix an grammatic error.

--- myapp.py
+++ myapp.py
@@ -11,4 +11,4 @@ def system_info():
     return f"<h1>System Information:</h1><p>System: {system}</p><p>Release: {release}</p><p>Version: {version}</p>"
 
 if __name__ == "__main__":
-    app.run(app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
